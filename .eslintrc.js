a8c7fcbee41b7ff4c501275caab5577ab4f480d3
Chris Dascoli <chris.dascoli@gmail.com>
2018-02-08 14:33:29 -0500

    eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
-  extends: 'airbnb',
-  parser: 'babel-eslint',
-  rules: {
-    'react/prefer-stateless-function': 0,
-    'react/jsx-filename-extension': 0,
-    'comma-dangle': 0,
-    'import/prefer-default-export': 0,
-    'no-tabs': 0,
-    'no-mixed-spaces-and-tabs': 0,
-    'react/prop-types': 0,
-    indent: 0
-  }
+	extends: 'airbnb',
+	parser: 'babel-eslint',
+	rules: {
+		'react/prefer-stateless-function': 0,
+		'react/jsx-filename-extension': 0,
+		'comma-dangle': 0,
+		'import/prefer-default-export': 0,
+		'no-tabs': 0,
+		'no-mixed-spaces-and-tabs': 0,
+		'react/prop-types': 0,
+		indent: 0,
+		radix: 0,
+		'max-len': 0
+	}
 };
